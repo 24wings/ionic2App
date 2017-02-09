b512d2aa1f4d4bd7b829bdcd6ae93c0caf81dbd7
24wings <2121718893@qq.com>
2017-02-09 14:22:07 +0800

    更多 已经完成 支持修改用户名,头像,昵称，标签,选择生日

--- src/pages/more/more.ts
+++ src/pages/more/more.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams, AlertController } from 'ionic-angular';
+import { NavController, NavParams, AlertController, ActionSheetController } from 'ionic-angular';
 
 
 
@@ -14,8 +14,13 @@ import { NavController, NavParams, AlertController } from 'ionic-angular';
   templateUrl: 'more.html'
 })
 export class MorePage {
-  customer = { username: '', tags: ['手术', '医疗'] };
-  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController) {
+  customer = {
+    avatar: 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTtz2uemEb7YMfqXFIfL6rQ8IXVnPZxDvqHN7lgBorPH29fCD3M1haaSpA',
+    username: '',
+    tags: ['美容', '美发', '去毛'],
+    workYears: 8
+  };
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController, public actionCtrl: ActionSheetController) {
 
   }
 
@@ -50,7 +55,70 @@ export class MorePage {
   }
 
   addTag() {
+    this.alertCtrl.create({
+      title: '新增标签',
+      inputs: [{
+        name: 'tag',
+        placeholder: '新标签的名字'
+      }],
+      buttons: [{
+        text: '取消'
+      }, {
+        text: '确定',
+        handler: (data) => {
+          this.customer.tags.push(data.tag);
+        }
+      }]
+    }).present();
 
 
   }
+
+  deleteTag(tag) {
+    this.customer.tags.splice(this.customer.tags.indexOf(tag), 1);
+    console.log(tag)
+  }
+
+  showAvatarAction(fileInput: HTMLInputElement) {
+    this.actionCtrl.create({
+      title: '修改头像',
+      buttons: [
+        {
+          text: '从图库中选取',
+
+          handler: () => {
+            fileInput.click();
+
+          }
+        }, {
+          text: '从手机相册中选取',
+          handler: () => {
+            fileInput.click();
+
+          }
+        }, {
+          text: '从聊天记录中选取',
+          handler: () => {
+            fileInput.click();
+          }
+        },
+        {
+          text: '取消',
+          role: 'cancel',
+        }
+      ]
+    }).present();
+  }
+
+  changeAvatar(fileInput: HTMLInputElement) {
+    var file = fileInput.files[0];
+    if (!file) return 0;
+    var reader = new FileReader();
+    reader.onload = (event) => {
+      this.customer.avatar = event.target['result'];
+      console.log(this.customer.avatar)
+    }
+    reader.readAsDataURL(file);
+  }
+
 }
